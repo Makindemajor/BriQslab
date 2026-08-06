fe96c2a94a6b40444192e35907efdce20ab47409
Makinde Oreoluwa <oreoluwamakinde07@gmail.com>
2026-08-06 16:59:47 +0100

    created frontend class codes

---
+++ Week 7/script.js
@@ -0,0 +1,14 @@
+alert("Welcome to my website!");
+console.log("Hello World!");
+let name = "John";
+console.log(name);
+
+let firstname = "Oreoluwa";
+let lastname = "Makinde";
+let age = 20;
+let state = "Ogun";
+
+console.log(firstname + " " + lastname);
+console.log("My name is" + " " + firstname + " "+ lastname);
+console.log("I am" + " " + age + " years old");
+console.log("I live in" + " " + state);
